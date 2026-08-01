e6ed9f7a0218a2f95ec9f01f6119a7feb8fe9740
FlammortDev <hiblotmarceau@gmail.com>
2026-08-01 02:58:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -143,6 +143,7 @@ function actualiserTousLesAmis(user1, user2) {
     });
 }
 
-server.listen(3000, () => {
-    console.log('Serveur lancé sur http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Serveur lancé sur le port ${PORT}`);
 });
